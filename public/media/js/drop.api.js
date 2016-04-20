d7eff1a21db0e6c6d1ca30b453cf09caaa659ed4
tanjusja <tanyxxxl91@mail.ru>
2016-04-20 21:50:08 +0300

    '2004'

--- public/media/js/drop.api.js
+++ public/media/js/drop.api.js
@@ -6,10 +6,13 @@ addEventListener ('load', initiate); //к селектору добавляем 
 		}
 		//source1 = document.getElementById('image'); //переменная для источника
 		//source1.addEventListener('dragstart',dragged); //прослушиватель для источника
-		drop = document.getElementById('dropbox'); //переменная для целевого документа
+		//drop = document.getElementById('dropbox'); //переменная для целевого документа
+		drop = document.getElementById('canvas');
 		drop.addEventListener('dragenter',enter);//прослушиватель  для целевого документа
 		drop.addEventListener('dragover',over); //по методу dragover срабатывает функция over
 		drop.addEventListener('drop',dropped);//отпустили элемент
+		
+		canvas = drop.getContext('2d');
 		
 	}	
 	function enter(e){
@@ -22,6 +25,7 @@ addEventListener ('load', initiate); //к селектору добавляем 
 	function dragged(e){
 		element = e.target; //то же самое что this - jquery
 		e.dataTransfer.setData('Text',element.getAttribute('id')); //передаем из целевой в источник
+		e.dataTransfer.setDragImage(element,0,0); //передаем картинку
 		/*var code='<img src= " '+source1.getAttribute('src')+' ">';
 		e.dataTransfer.setData('Text',code);//объявляет переменную для передачи целевой*/
 		drop.style.background = 'yellow';
@@ -29,11 +33,21 @@ addEventListener ('load', initiate); //к селектору добавляем 
 	function dropped(e){
 		e.preventDefault(); //отмена действия по умолчанию
 		var id = e.dataTransfer.getData('Text');
-		var src = document.getElementById(id).src;
-		var img = '<img src = "'+src+'"/>';
 		
-		drop.innerHTML += img;
+		var img = document.createElement('img');
+		var src = document.getElementById(id).src;
+		//var img = '<img src = "'+src+'"/>';
+		img.setAttribute('src', src);
+		var posx = e.pageX - drop.offsetLeft;
+		var posy = e.pageY - drop.offsetTop;
+		
+		/*drop.innerHTML += img;*/
 		/*drop.innerHTML=e.dataTransfer.getData('Text'); //перехват данных*/
+		img.addEventListener('load', function(){
+			console.log(img);
+			canvas.drawImage(img, posx, posy);
+			});
+		
 		drop.style.background = 'red';
 		drop.style.color = 'red';
 	}
